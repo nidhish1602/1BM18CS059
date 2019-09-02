5f78568b3f0fcc716584ba5178b51d548df09fe5
Nidhish Lakhinana <47395841+nidhish1602@users.noreply.github.com>
2019-09-02 20:08:41 +0530

    Update que.c

--- que.c
+++ que.c
@@ -1,75 +1,70 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<conio.h>
+#include<process.h>
 
-const int maxsize=10;
 
-void enque(int a, int r, int que[maxsize])
+void enque(int x, int *rear, int a[10])
 {
-    printf("Enter the element");
-    scanf("%d", &a);
-    if(r==maxsize)
-        printf("Que is full");
-    else{
-        r++;
-        que[r]==a;
-    }
-
+	if(*rear == 29)
+	{
+		printf("Stack is FULL\n");
+	}
+	else
+	{
+		*rear+=1;
+		a[*rear] = x;
+	}
 }
-
-void deque(int r, int f, int que[maxsize])
+void deque(int *front, int *rear)
 {
-    if(r==f)
-        printf("Queue is empty");
-    else{
-    int b;
-    b=que[f];
-    int x=f+1;
-    while(x<=r)
-        {
-            que[x-1]=que[x];
-            x++;
-        }
-    }
+	if(*rear == (*front -1))
+	{
+		printf("Empty\n");
+	}
+	else if(*front == *rear)
+	{
+		*front = -1;
+		*rear = -1;
+	}
+	else
+	{
+		*front+=1;
+	}
 }
-
-/*void accept(int i, int n, int que[maxsize], int r)
+void print(int front, int rear, int a[10])
 {
-    do{
-        scanf("%d", &que[i]);
-    }while(i<n);
-    r=i;
-}*/
-
-void display(int f, int r, int que[maxsize])
-{
-    //for(int i=f, i<=r, i++)
-    int i=f;
-    while(i<=r)
-        {
-            printf("%d", que[i]);
-            i++;
-        }
+	int i;
+	for(i=front;i<=rear;i++)
+	printf("%d\t",a[i]);
 }
-
 int main()
 {
-    int f=0, r, que[maxsize], n, a, i=0, c=1, p=0;
-    while(c=4)
-    {
-    printf("\nMenu");
-    printf("\n 1.add\n 2.remove\n 3.Display\n");
-    scanf("%d", &c);
-    if(c=1)
-    {
-        enque(a, r, que);
-    }
-    else if(c=2)
-    {
-        deque(r, f, que);
-    }
-    else if(c=3)
-    {
-        display(f, r, que);
-    }
-    }
+	int choice, value, i;
+	int a[30], rear= -1, front=0;
+
+	clrscr();
+	for(;;)
+	{
+		printf("\n\nChoose your choice\n");
+		printf("	Press 1 to Insert\n");
+		printf("	Press 2 to Delete\n");
+		printf("	Press 3 to Display\n");
+		printf("	Press 4 to EXIT\n");
+		scanf("%d",&choice);
+		switch(choice)
+		{
+			case 1 : printf("Enter the value to be inserted into the queue\n");
+				 scanf("%d",&value);
+				 enque(value, &rear, a);
+				 print();
+				 break;
+			case 2 : deque(&front, &rear);
+				 break;
+			case 3 : print(front,rear,a);
+				 break;
+			case 4 : return 0;
+				break;
+		}
+	}
+	getch();
 }
